c251f2620ef618f22aa10dee9808753435bb68e7
Ian Dewsbury <ianmichaeld@gmail.com>
2018-01-05 18:32:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function shout(str){
 function whisper(str){
   return str.toLowerCase();
 }
+
+function logShout(str){
+  console.log(shout(str));
+}
+
+function logWhisper(str){
+  console.log(whisper(str));
+}
