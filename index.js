66af0c8ce8281fda7e9abfb0b8a289300e4f531e
Ian Dewsbury <ianmichaeld@gmail.com>
2018-01-05 18:40:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(str){
 }
 
 function sayHiToGrandma(str){
-  if(str === str.toLowerCase){
+  if(str === str.toLowerCase()){
     return "I can't hear you!";
-  } else if(str === str.toUpperCase){
+  } else if(str === str.toUpperCase()){
     return "YES INDEED!";
   } else if(str === "I love you, Grandma."){
     return "I love you, too.";
